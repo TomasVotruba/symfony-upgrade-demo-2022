2e3d88e04774756f3630199439e063beca2f9f84
Tomas Votruba <tomas.vot@gmail.com>
2022-11-18 11:16:25 +0100

    add the set list

--- rector-ci.php
+++ rector-ci.php
@@ -3,7 +3,8 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Symfony\Set\SymfonyLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(
@@ -12,4 +13,10 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(
         __DIR__ . '/rector-attributes.php'
     );
+
+    // "the last upgrade"
+    $rectorConfig->sets([
+        // LevelSetList::UP_TO_PHP_81,
+        // SymfonyLevelSetList::UP_TO_SYMFONY_61
+    ]);
 };
